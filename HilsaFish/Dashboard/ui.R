e4bb95ac7e3048420b90c3a108032fcde187de31
Xiuyang Guan <xiuyangguan@gmail.com>
2017-03-21 13:16:11 -0400

    Add files via upload

--- HilsaFish/Dashboard/ui.R
+++ HilsaFish/Dashboard/ui.R
@@ -1,44 +1,25 @@
-# Load the ggplot2 package which provides
-# the 'mpg' dataset.
 library(ggplot2)
 library(leaflet)
 library(ggvis)
 library(dplyr)
-load("~/Google Drive/Xunyang_Fall16Spring17/Hilsa fish research/3.Analysis/RScript/shinyapp/data/WaterLevelTimeSeries_No_Human.RData")
-load("~/Google Drive/Xunyang_Fall16Spring17/Hilsa fish research/3.Analysis/RScript/shinyapp/data/WaterLevelTimeSeries_All.RData")
-load("~/Google Drive/Xunyang_Fall16Spring17/Hilsa fish research/3.Analysis/RScript/shinyapp/data/Overview_DATA.RData")
-Input_path <- "~/Google Drive/Xunyang_Fall16Spring17/Hilsa fish research/2.Data"
+load("data/WaterLevelTimeSeries_No_Human.RData")
+load("data/WaterLevelTimeSeries_All.RData")
+load("data/Overview_DATA.RData")
 
 Names <- names(WaterLevel_No_Human_Mean_Monsoon)[2:6]
 Names_NoHuman <- c("Bahadurabad", "Chilmari", "Kanaighat", "Sarighat", "Sherpur")
 
-Analytics_Types <- c("Year vs. Monsoon",
-                     "Year vs. NonMonsoon",
-                     "Year vs. Production",
-                     "Monsoon vs. Production",
-                     "Monsoon vs. NonMonsoon"
-)
+Plot_Explorer_Input <- c("Year", "Monsoon", "Non_Monsoon", "Production")
 
 Locations <- names(WaterLevel_All_Mean_Monsoon)[-1]
 
-Production_Locations <- c("Noakhali"   ,             "Bhola"       ,            "Barisal"      ,          
-                           "Lakshmipur"    ,          "Sariatpur"    ,           "Chandpur"    ,           
-                           "Munshiganj"       ,       "Narayanganj"   ,          "Comilla"    ,            
-                           "Narshingdi"       ,       "Brahmanbaria"  ,        "Kishoreganj" ,           
-                           "Habiganj"      ,          "Sariatpur.1"  ,           "Madaripur"   ,           
-                           "Munshiganj.1"   ,         "Dhaka"       ,            "Manikganj"   ,           
-                           "Faridpur"       ,         "Rajbari"   ,              "Rajbari.1"  ,            
-                           "Kushtia"       ,          "Pabna"     ,              "Natore"     ,            
-                           "Rajshahi"     ,           "C..Nawabganj" ,           "Manikganj.1" ,           
-                           "Pabna.1"    ,             "Tangail"      ,           "Sirajganj"   ,           
-                           "Bogra"     ,              "Jamalpur"     ,           "Gaibandha"   ,           
-                           "Jamalpur.1"    ,          "Gaibandha.1"  ,           "Kurigram")
-
+Production_Locations <- unique(Overview_DATA[Overview_DATA$Types == "Production",]$variable)
+  
 navbarPage("Hilsa Fish",
            # Interactive map
-           tabPanel("About",
-                    h1("Introduction")
-                    ),
+#            tabPanel("About",
+#                     h1("Introduction")
+#            ),
            tabPanel("Interactive Map",
                     div(class="outer",
                         tags$head(
@@ -60,7 +41,7 @@ navbarPage("Hilsa Fish",
                                       
                                       checkboxGroupInput('variables', 'Analytical Variables:',
                                                          c("Monsoon", "Non_Monsoon", "Production"), selected = "Monsoon"),
-
+                                      
                                       selectInput("location", "Location", Names_NoHuman),
                                       sliderInput("year", "Year", 1970, 2016, value = 1970, step = 1),
                                       selectInput("size", "Size", Names_NoHuman, selected = "adultpop")
@@ -68,7 +49,7 @@ navbarPage("Hilsa Fish",
                         )
                     )
                     
-
+                    
            ),
            
            
@@ -77,10 +58,13 @@ navbarPage("Hilsa Fish",
                       column(3,
                              wellPanel(
                                sliderInput("year", "Year Range", 1970, 2016, value = c(1970, 2016)),
-                               selectInput("analyticsTypes", "Analytical Types", Analytics_Types, selected = "Year vs. Monsoon"), 
+                               checkboxGroupInput('Plot_Options', 'Options',
+                                                  c("Abline")),
+                               selectInput("plot_x_input", "x_axis", Plot_Explorer_Input, selected = "Year"),
+                               selectInput("plot_y_input", "y_axis", Plot_Explorer_Input, selected = "Monsoon"),
                                selectInput("targetLocation_WaterLevel", "Target Location Water Level", Locations, selected = "Amalshid"),
-                               selectInput("targetLocation_Production", "Target Location Production", Production_Locations, selected = "Noakhali")
-                               )
+                               selectInput("targetLocation_Production", "Target Location Production", Production_Locations, selected = "Bangladesh_Inland")
+                             )
                              
                       ),
                       mainPanel(
@@ -89,11 +73,11 @@ navbarPage("Hilsa Fish",
                     )
                     
                     
-#                     checkboxGroupInput("checkGroup", label = h3("Checkbox group"), 
-#                                        choices = list("Bahadurabad" = "Bahadurabad", "Chilmari" = "Chilmari", "Kanaighat" = "Kanaighat", "Sarighat" = "Sarighat", "Sherpur" = "Sherpur"),
-#                                        selected = 1)
-
-              ),
+                    #                     checkboxGroupInput("checkGroup", label = h3("Checkbox group"), 
+                    #                                        choices = list("Bahadurabad" = "Bahadurabad", "Chilmari" = "Chilmari", "Kanaighat" = "Kanaighat", "Sarighat" = "Sarighat", "Sherpur" = "Sherpur"),
+                    #                                        selected = 1)
+                    
+           ),
            
            tabPanel("Data Explorer",
                     sidebarLayout(
@@ -105,8 +89,5 @@ navbarPage("Hilsa Fish",
                       )
                     )
            )
-
+           
 )
-
-
-
